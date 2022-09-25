8c2360a5d2dc468594733c2bad1c44509f0f26a1
DimaLifeIsGood <dimonvarauka@yahoo.com>
2022-09-25 21:58:01 +0200

    firstprogramm

---
+++ src/Begin.java
@@ -0,0 +1,16 @@
+public class Begin {
+    public static void main(String[] args){
+        long n=5;
+        boolean d=true;
+        Character ch='z';
+        char z=ch;
+        System.out.println("Привет всем ребята    "+(float)n);
+        System.out.println(n);
+        System.out.println(d);
+        System.out.println(ch);
+        Integer a=new Integer(7);
+        System.out.println(a);
+
+    };
+}
+
